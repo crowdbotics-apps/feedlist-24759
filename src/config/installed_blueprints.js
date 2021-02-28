f532564bef61d6e4dbba77b0114653df687f5c11
Crowdbotics <team@crowdbotics.com>
2021-02-28 19:11:18 +0000

    Update components

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -3,4 +3,9 @@ export const installed_blueprints = [
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
-];
+  {
+    name: "LoginInputAutocomplete0209962",
+    human_name: "LoginInputAutocomplete0209962",
+    access_route: "LoginInputAutocomplete0209962"
+  }
+]
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -1,34 +1,36 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import {createDrawerNavigator} from 'react-navigation-drawer';
+import { createAppContainer } from "react-navigation"
+import { createStackNavigator } from "react-navigation-stack"
+import { createDrawerNavigator } from "react-navigation-drawer"
 
-import SplashScreen from "../features/SplashScreen";
-import SideMenu from './sideMenu';
+import SplashScreen from "../features/SplashScreen"
+import SideMenu from "./sideMenu"
 //@BlueprintImportInsertion
 
 /**
  * new navigators can be imported here
- */
+ */ import LoginInputAutocomplete0209962 from "../features/LoginInputAutocomplete0209962/navigator"
 
 const AppNavigator = {
+  LoginInputAutocomplete0209962: { screen: LoginInputAutocomplete0209962 },
 
-    //@BlueprintNavigationInsertion
+  //@BlueprintNavigationInsertion
 
-    /** new navigators can be added here */
-    SplashScreen: {
-      screen: SplashScreen
-    }
-};
+  /** new navigators can be added here */
+  SplashScreen: {
+    screen: SplashScreen
+  }
+}
 
 const DrawerAppNavigator = createDrawerNavigator(
   {
-    ...AppNavigator,
+    ...AppNavigator
   },
+
   {
     contentComponent: SideMenu
-  },
-);
+  }
+)
 
-const AppContainer = createAppContainer(DrawerAppNavigator);
+const AppContainer = createAppContainer(DrawerAppNavigator)
 
-export default AppContainer;
+export default AppContainer
